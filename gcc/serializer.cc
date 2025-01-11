2e1b512e37bb58a2e50b48d2efb1b064debbc942
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-01-10 19:44:36 -0600

    Hook placement; start of serializer class

--- gcc/serializer.cc
+++ gcc/serializer.cc
@@ -7,6 +7,69 @@
  *  For different objects.*/
 
 #include "tree-emit-json.h"
+#include "gimple-emit-json.h"
+#include "rtl-emit-json.h"
+
+template<typename T>
+static void
+dequeue_and_add (visitor *di)
+{
+  dump_queue_p dq;
+  splay_tree_node stn;
+  T t;
+
+  /* Get the next node from the queue.  */
+  dq = di->queue;
+  stn = dq->node;
+  t = (tree) stn->key;
+
+  /* Remove the node from the queue, and put it on the free list.  */
+  di->queue = dq->next;
+  if (!di->queue)
+    di->queue_end = 0;
+  dq->next = di->free_list;
+  di->free_list = dq;
+
+  /* Convert the node to JSON and store it to be dumped later. */
+  auto dummy = node_emit_json(t, di).release();
+  di->json_dump->append(dummy);
+}
+
+template<typename T>
+static void
+queue (visitor *di, T node)
+{
+  visitor_queue *dq;
+
+  /* Obtain a new queue node.  */
+  if (di->free_list)
+    {
+      dq = di->free_list;
+      di->free_list = dq->next;
+    }
+  else
+    dq = XNEW (struct visitor_queue);
+
+  /* Create a new entry in the splay-tree and insert into queue iff new.
+   * Else, end.*/
+  if (!splay_tree_lookup (di->nodes, (splay_tree_key) node))
+  {
+    dq->node = splay_tree_insert (di->nodes, (splay_tree_key) t,
+          			(splay_tree_value) dni);
+    dq->next = 0;
+    if (!di->queue_end)
+      di->queue = dq;
+    else
+      di->queue_end->next = dq;
+    di->queue_end = dq;
+  }
+}
+
+template <typename T> void
+execute_per_node (T node, void func)
+{
+
+}
 
 tree_json_writer::tree_json_writer ()
   : m_json_root_tuple ()
--- gcc/serializer.h
+++ gcc/serializer.h
@@ -1,5 +1,29 @@
 /* Serialization of some data structures to JSON. */
 
+#include "typed-splay-tree.h"
+
+template<typename T>
+class visitor
+{
+public:
+  FILE *stream;
+  const T node;
+  dump_flags_t flags;
+  visitor_queue_p dump_queue;
+  visitor_queue_p queue_end;
+  visitor_queue_p free_list;
+  typed_splay_tree<void*,  T> nodes;
+
+  void dequeue_and_add(T);
+  void queue(T);
+};
+
+struct visitor_queue 
+{
+  splay_tree_node<T> node;
+  visitor_queue *next;
+};
+
 class tree_json_writer
 {
 public:
@@ -7,7 +31,7 @@ public:
   ~tree_json_writer ();
   void write (FILE * file);
   void add_node (tree t);
-  void add_fndecl_tree (tree fndecl, dump_flags_t flags);
+  void add_fndecl_tree (const_tree fndecl, dump_flags_t flags);
   void set_stream (tree_dump_index tdi);
 
 private:
