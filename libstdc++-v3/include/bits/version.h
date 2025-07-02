a496e4419b5d5126d9cb03fa610e53cad8e65f18
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-07-02 11:59:50 -0500

    Start feature test macros - check if need hosted or freestanding ok

--- libstdc++-v3/include/bits/version.h
+++ libstdc++-v3/include/bits/version.h
@@ -2273,4 +2273,116 @@
 #endif /* !defined(__cpp_lib_exception_ptr_cast) && defined(__glibcxx_want_exception_ptr_cast) */
 #undef __glibcxx_want_exception_ptr_cast
 
+// TODO: I'm fairly sure that we need _GLIBCXX_HOSTED too
+
+#if !defined(_cpp_lib_constexpr_deque)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_deque // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_deque)
+#   define __cpp_lib_constexpr_deque 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_deque
+
+#if !defined(_cpp_lib_constexpr_flat_map)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_flat_map // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_flat_map)
+#   define __cpp_lib_constexpr_flat_map 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_flat_map
+
+#if !defined(_cpp_lib_constexpr_flat_set)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_flat_set // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_flat_set)
+#   define __cpp_lib_constexpr_flat_set 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_flat_set
+
+#if !defined(_cpp_lib_constexpr_forward_list)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_forward_list // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_forward_list)
+#   define __cpp_lib_constexpr_forward_list 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_forward_list
+
+#if !defined(_cpp_lib_constexpr_list)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_list // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_list)
+#   define __cpp_lib_constexpr_list 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_list
+
+#if !defined(_cpp_lib_constexpr_map)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_map // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_map)
+#   define __cpp_lib_constexpr_map 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_map
+
+#if !defined(_cpp_lib_constexpr_queue)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_queue // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_queue)
+#   define __cpp_lib_constexpr_queue 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_queue
+
+#if !defined(_cpp_lib_constexpr_set)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_set // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_set)
+#   define __cpp_lib_constexpr_set 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_set
+
+#if !defined(_cpp_lib_constexpr_stack)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_stack // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_stack)
+#   define __cpp_lib_constexpr_stack 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_stack
+
+#if !defined(_cpp_lib_constexpr_unordered_map)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_unordered_map // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_unordered_map)
+#   define __cpp_lib_constexpr_unordered_map 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_unordered_map
+
+#if !defined(_cpp_lib_constexpr_unordered_set)
+# if (__cplusplus > 202502L)
+#  define __glibcxx_constexpr_unordered_set // TODO: idk lol
+#  if defined(__glibcxx_want_all) || defined(__glibcxx_want_constexpr_unordered_set)
+#   define __cpp_lib_constexpr_unordered_set 202502L
+#  #endif
+# endif
+#endif
+#undef __glibcxx_want_constexpr_unordered_set
+
 #undef __glibcxx_want_all
