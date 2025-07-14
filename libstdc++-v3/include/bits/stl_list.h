8a96a3d022aee8ee86af7b03e98d7b1e3180beeb
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-07-14 14:51:22 -0500

    Abi breakage maybe

--- libstdc++-v3/include/bits/stl_list.h
+++ libstdc++-v3/include/bits/stl_list.h
@@ -554,10 +554,10 @@ _GLIBCXX_BEGIN_NAMESPACE_CONTAINER
       typedef _List_node* _Node_ptr;
 
 #if __cplusplus >= 201103L
-      alignas(_Tp ) typedef union
+      typedef union
 	{
 	     _Tp _M_storage;
-	} _Uninit_storage __attribute__ ((__aligned_as(alignof(_Tp))__));
+	} _Uninit_storage __attribute__ ((aligned(alignof(_Tp))));
       _Uninit_storage __u;
       _Tp*	 _M_valptr()	   { return __u._M_storage;}
       _Tp const* _M_valptr() const { return __u._M_storage;}
