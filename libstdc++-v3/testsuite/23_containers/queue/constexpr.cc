167402911f02722c3c234a24f042836e7573a9fe
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-07-16 16:53:11 -0400

    queue constexpr test start

---
+++ libstdc++-v3/testsuite/23_containers/queue/constexpr.cc
@@ -0,0 +1,32 @@
+#include <deque>
+
+#ifndef __cpp_lib_constexpr_deque
+//Should we do something here?
+#endif
+
+constexpr int push_and_pop_test() {
+  std::queue<int> tmp;
+  tmp.push(1);
+  tmp.push(2);
+  tmp.push(3);
+
+  int a = tmp.front();
+  tmp.pop();
+  a += tmp.front();
+
+  return a;
+}
+
+constexpr void move_test()
+{
+  std::queue<int> a,b;
+  a.push(1);
+  b = std::move(a);
+  static_assert( b.size() == 1 && b.front() == 1 && a.size() == 0 );
+
+  std::queue<int> c(std::move(b));
+  static_assert( c.size() == 1 && c.front() == 1 );
+  static_assert( b.size() == 0 );
+}
+
+static_assert(queue_test() = 1 + 2);
