3db84aef1a45ebf77f210d8086c65c7f0f3fc86e
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-08-05 09:42:36 -0400

    fix deque missed constexpr

--- libstdc++-v3/include/bits/stl_deque.h
+++ libstdc++-v3/include/bits/stl_deque.h
@@ -2086,15 +2086,15 @@ _GLIBCXX_BEGIN_NAMESPACE_CONTAINER
        *  push_back on each value from the iterator.
        */
       template<typename _InputIterator>
-	GLIBCXX26_CONSTEXPR
 	void
+	GLIBCXX26_CONSTEXPR
 	_M_range_initialize(_InputIterator __first, _InputIterator __last,
 			    std::input_iterator_tag);
 
       // called by the second initialize_dispatch above
       template<typename _ForwardIterator>
-	GLIBCXX26_CONSTEXPR
 	void
+	GLIBCXX26_CONSTEXPR
 	_M_range_initialize(_ForwardIterator __first, _ForwardIterator __last,
 			    std::forward_iterator_tag);
       ///@}
