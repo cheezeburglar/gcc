4ce8a077a1e120c789c899d1a2378d3e0248e3c3
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-08-21 15:45:12 -0400

    hash internals

--- libstdc++-v3/include/bits/hashtable.h
+++ libstdc++-v3/include/bits/hashtable.h
@@ -379,6 +379,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // numerous checks in the code to avoid 0 modulus.
       __node_base_ptr		_M_single_bucket	= nullptr;
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_update_bbegin()
       {
@@ -386,6 +387,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	  _M_buckets[_M_bucket_index(*__begin)] = &_M_before_begin;
       }
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_update_bbegin(__node_ptr __n)
       {
@@ -393,10 +395,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	_M_update_bbegin();
       }
 
+      _GLIBCXX26_CONSTEXPR
       bool
       _M_uses_single_bucket(__buckets_ptr __bkts) const
       { return __builtin_expect(__bkts == &_M_single_bucket, false); }
 
+      _GLIBCXX26_CONSTEXPR
       bool
       _M_uses_single_bucket() const
       { return _M_uses_single_bucket(_M_buckets); }
@@ -408,9 +412,11 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	  __detail::_Hashtable_hash_traits<_Hash>::__small_size_threshold();
       }
 
+      _GLIBCXX26_CONSTEXPR
       __hashtable_alloc&
       _M_base_alloc() { return *this; }
 
+      _GLIBCXX26_CONSTEXPR
       __buckets_ptr
       _M_allocate_buckets(size_type __bkt_count)
       {
@@ -423,6 +429,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	return __hashtable_alloc::_M_allocate_buckets(__bkt_count);
       }
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_deallocate_buckets(__buckets_ptr __bkts, size_type __bkt_count)
       {
@@ -432,12 +439,14 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	__hashtable_alloc::_M_deallocate_buckets(__bkts, __bkt_count);
       }
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_deallocate_buckets()
       { _M_deallocate_buckets(_M_buckets, _M_bucket_count); }
 
       // Gets bucket begin, deals with the fact that non-empty buckets contain
       // their before begin node.
+      _GLIBCXX26_CONSTEXPR
       __node_ptr
       _M_bucket_begin(size_type __bkt) const
       {
@@ -445,6 +454,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	return __n ? static_cast<__node_ptr>(__n->_M_nxt) : nullptr;
       }
 
+      _GLIBCXX26_CONSTEXPR
       __node_ptr
       _M_begin() const
       { return static_cast<__node_ptr>(_M_before_begin._M_nxt); }
@@ -452,10 +462,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // Assign *this using another _Hashtable instance. Whether elements
       // are copied or moved depends on the _Ht reference.
       template<typename _Ht>
+	_GLIBCXX26_CONSTEXPR
 	void
 	_M_assign_elements(_Ht&&);
 
       template<typename _Ht>
+	_GLIBCXX26_CONSTEXPR
 	void
 	_M_assign(_Ht&& __ht)
 	{
@@ -464,15 +476,19 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	}
 
       template<typename _Ht, typename _NodeGenerator>
+	_GLIBCXX26_CONSTEXPR
 	void
 	_M_assign(_Ht&&, _NodeGenerator&);
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_move_assign(_Hashtable&&, true_type);
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_move_assign(_Hashtable&&, false_type);
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_reset() noexcept;
 
@@ -499,20 +515,24 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 #endif
 	}
 
+      _GLIBCXX26_CONSTEXPR
       _Hashtable(_Hashtable&& __ht, __node_alloc_type&& __a,
 		 true_type /* alloc always equal */)
 	noexcept(_S_nothrow_move());
 
+      _GLIBCXX26_CONSTEXPR
       _Hashtable(_Hashtable&&, __node_alloc_type&&,
 		 false_type /* alloc always equal */);
 
       template<typename _InputIterator>
+	_GLIBCXX26_CONSTEXPR
 	_Hashtable(_InputIterator __first, _InputIterator __last,
 		   size_type __bkt_count_hint,
 		   const _Hash&, const _Equal&, const allocator_type&,
 		   true_type __uks);
 
       template<typename _InputIterator>
+	_GLIBCXX26_CONSTEXPR
 	_Hashtable(_InputIterator __first, _InputIterator __last,
 		   size_type __bkt_count_hint,
 		   const _Hash&, const _Equal&, const allocator_type&,
@@ -520,12 +540,16 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
     public:
       // Constructor, destructor, assignment, swap
+      _GLIBCXX26_CONSTEXPR
       _Hashtable() = default;
 
+      _GLIBCXX26_CONSTEXPR
       _Hashtable(const _Hashtable&);
 
+      _GLIBCXX26_CONSTEXPR
       _Hashtable(const _Hashtable&, const allocator_type&);
 
+      _GLIBCXX26_CONSTEXPR
       explicit
       _Hashtable(size_type __bkt_count_hint,
 		 const _Hash& __hf = _Hash(),
@@ -533,18 +557,21 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 		 const allocator_type& __a = allocator_type());
 
       // Use delegating constructors.
+      _GLIBCXX26_CONSTEXPR
       _Hashtable(_Hashtable&& __ht)
 	noexcept(_S_nothrow_move())
       : _Hashtable(std::move(__ht), std::move(__ht._M_node_allocator()),
 		   true_type{})
       { }
 
+      _GLIBCXX26_CONSTEXPR
       _Hashtable(_Hashtable&& __ht, const allocator_type& __a)
 	noexcept(_S_nothrow_move<__node_alloc_traits::_S_always_equal()>())
       : _Hashtable(std::move(__ht), __node_alloc_type(__a),
 		   typename __node_alloc_traits::is_always_equal{})
       { }
 
+      _GLIBCXX26_CONSTEXPR
       explicit
       _Hashtable(const allocator_type& __a)
       : __hashtable_alloc(__node_alloc_type(__a)),
@@ -552,6 +579,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       { }
 
       template<typename _InputIterator>
+	_GLIBCXX26_CONSTEXPR
 	_Hashtable(_InputIterator __f, _InputIterator __l,
 		   size_type __bkt_count_hint = 0,
 		   const _Hash& __hf = _Hash(),
@@ -561,6 +589,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 		     __unique_keys{})
 	{ }
 
+      _GLIBCXX26_CONSTEXPR
       _Hashtable(initializer_list<value_type> __l,
 		 size_type __bkt_count_hint = 0,
 		 const _Hash& __hf = _Hash(),
@@ -571,10 +600,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       { }
 
 
+      _GLIBCXX26_CONSTEXPR
       _Hashtable&
       operator=(const _Hashtable& __ht);
 
 
+      _GLIBCXX26_CONSTEXPR
       _Hashtable&
       operator=(_Hashtable&& __ht)
       noexcept(__node_alloc_traits::_S_nothrow_move()
@@ -755,6 +786,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       cend(size_type __bkt) const
       { return const_local_iterator(*this, nullptr, __bkt, _M_bucket_count); }
 
+      _GLIBCXX26_CONSTEXPR
       float
       load_factor() const noexcept
       {
@@ -766,27 +798,34 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // Generalization of max_load_factor.  Extension, not found in
       // TR1.  Only useful if _RehashPolicy is something other than
       // the default.
+      _GLIBCXX26_CONSTEXPR
       const _RehashPolicy&
       __rehash_policy() const
       { return _M_rehash_policy; }
 
+      _GLIBCXX26_CONSTEXPR
       void
       __rehash_policy(const _RehashPolicy& __pol)
       { _M_rehash_policy = __pol; }
 
       // Lookup.
+      _GLIBCXX26_CONSTEXPR
       iterator
       find(const key_type& __k);
 
+      _GLIBCXX26_CONSTEXPR
       const_iterator
       find(const key_type& __k) const;
 
+      _GLIBCXX26_CONSTEXPR
       size_type
       count(const key_type& __k) const;
 
+      _GLIBCXX26_CONSTEXPR
       std::pair<iterator, iterator>
       equal_range(const key_type& __k);
 
+      _GLIBCXX26_CONSTEXPR
       std::pair<const_iterator, const_iterator>
       equal_range(const key_type& __k) const;
 
@@ -794,42 +833,50 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       template<typename _Kt,
 	       typename = __has_is_transparent_t<_Hash, _Kt>,
 	       typename = __has_is_transparent_t<_Equal, _Kt>>
+	_GLIBCXX26_CONSTEXPR
 	iterator
 	_M_find_tr(const _Kt& __k);
 
       template<typename _Kt,
 	       typename = __has_is_transparent_t<_Hash, _Kt>,
 	       typename = __has_is_transparent_t<_Equal, _Kt>>
+	_GLIBCXX26_CONSTEXPR
 	const_iterator
 	_M_find_tr(const _Kt& __k) const;
 
       template<typename _Kt,
 	       typename = __has_is_transparent_t<_Hash, _Kt>,
 	       typename = __has_is_transparent_t<_Equal, _Kt>>
+	_GLIBCXX26_CONSTEXPR
 	size_type
 	_M_count_tr(const _Kt& __k) const;
 
       template<typename _Kt,
 	       typename = __has_is_transparent_t<_Hash, _Kt>,
 	       typename = __has_is_transparent_t<_Equal, _Kt>>
+	_GLIBCXX26_CONSTEXPR
 	pair<iterator, iterator>
 	_M_equal_range_tr(const _Kt& __k);
 
       template<typename _Kt,
 	       typename = __has_is_transparent_t<_Hash, _Kt>,
 	       typename = __has_is_transparent_t<_Equal, _Kt>>
+	_GLIBCXX26_CONSTEXPR
 	pair<const_iterator, const_iterator>
 	_M_equal_range_tr(const _Kt& __k) const;
 #endif // __glibcxx_generic_unordered_lookup
 
+      _GLIBCXX26_CONSTEXPR
       void _M_rehash_insert(size_type __n);
 
     private:
       // Bucket index computation helpers.
+      _GLIBCXX26_CONSTEXPR
       size_type
       _M_bucket_index(const __node_value_type& __n) const noexcept
       { return __hash_code_base::_M_bucket_index(__n, _M_bucket_count); }
 
+      _GLIBCXX26_CONSTEXPR
       size_type
       _M_bucket_index(__hash_code __c) const
       { return __hash_code_base::_M_bucket_index(__c, _M_bucket_count); }
@@ -839,6 +886,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // Get hash code for a node that comes from another _Hashtable.
       // Reuse a cached hash code if the hash function is stateless,
       // otherwise recalculate it using our own hash function.
+      _GLIBCXX26_CONSTEXPR
       __hash_code
       _M_hash_code_ext(const __node_value_type& __from) const
       {
@@ -850,10 +898,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
       // Like _M_bucket_index but when the node is coming from another
       // container instance.
+      _GLIBCXX26_CONSTEXPR
       size_type
       _M_bucket_index_ext(const __node_value_type& __from) const
       { return _RangeHash{}(_M_hash_code_ext(__from), _M_bucket_count); }
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_copy_code(__node_value_type& __to,
 		   const __node_value_type& __from) const
@@ -862,6 +912,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 	  __to._M_hash_code = _M_hash_code_ext(__from);
       }
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_store_code(__node_value_type& __to, __hash_code __code) const
       {
@@ -873,10 +924,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // Find and insert helper functions and types
 
       // Find the node before the one matching the criteria.
+      _GLIBCXX26_CONSTEXPR
       __node_base_ptr
       _M_find_before_node(size_type, const key_type&, __hash_code) const;
 
       template<typename _Kt>
+	_GLIBCXX26_CONSTEXPR
 	__node_base_ptr
 	_M_find_before_node_tr(size_type, const _Kt&, __hash_code) const;
 
@@ -885,18 +938,22 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       struct __location_type
       {
 	// True if _M_node() is a valid node pointer.
+	_GLIBCXX26_CONSTEXPR
 	explicit operator bool() const noexcept
 	{ return static_cast<bool>(_M_before); }
 
 	// An iterator that refers to the node, or end().
+	_GLIBCXX26_CONSTEXPR
 	explicit operator iterator() const noexcept
 	{ return iterator(_M_node()); }
 
 	// A const_iterator that refers to the node, or cend().
+	_GLIBCXX26_CONSTEXPR
 	explicit operator const_iterator() const noexcept
 	{ return const_iterator(_M_node()); }
 
 	// A pointer to the node, or null.
+	_GLIBCXX26_CONSTEXPR
 	__node_ptr _M_node() const
 	{
 	  if (_M_before)
@@ -924,10 +981,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // cast to __node_ptr, and it must not be used to modify *_M_before
       // except in non-const member functions, such as erase.
 
+      _GLIBCXX26_CONSTEXPR
       __location_type
       _M_locate(const key_type& __k) const;
 
 
+      _GLIBCXX26_CONSTEXPR
       __node_ptr
       _M_find_node(size_type __bkt, const key_type& __key,
 		   __hash_code __c) const
@@ -939,6 +998,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
       template<typename _Kt>
 
+	_GLIBCXX26_CONSTEXPR
 	__node_ptr
 	_M_find_node_tr(size_type __bkt, const _Kt& __key,
 			__hash_code __c) const
@@ -950,6 +1010,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
       // Insert a node at the beginning of a bucket.
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_insert_bucket_begin(size_type __bkt, __node_ptr __node)
       {
@@ -979,6 +1040,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
       // Remove the bucket first node
 
+      _GLIBCXX26_CONSTEXPR
       void
       _M_remove_bucket_begin(size_type __bkt, __node_ptr __next_n,
 			     size_type __next_bkt)
@@ -994,10 +1056,12 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
 
       // Get the node before __n in the bucket __bkt
 
+      _GLIBCXX26_CONSTEXPR
       __node_base_ptr
       _M_get_previous_node(size_type __bkt, __node_ptr __n);
 
 
+      _GLIBCXX26_CONSTEXPR
       pair<__node_ptr, __hash_code>
       _M_compute_hash_code(__node_ptr __hint, const key_type& __k) const;
 
@@ -1008,6 +1072,7 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // __n_elt is an estimated number of elements we expect to insert,
       // used as a hint for rehashing when inserting a range.
 
+      _GLIBCXX26_CONSTEXPR
       iterator
       _M_insert_unique_node(size_type __bkt, __hash_code,
 			    __node_ptr __n, size_type __n_elt = 1);
@@ -1015,12 +1080,14 @@ _GLIBCXX_BEGIN_NAMESPACE_VERSION
       // Insert node __n with key __k and hash code __code.
       // Takes ownership of __n if insertion succeeds, throws otherwise.
 
+      _GLIBCXX26_CONSTEXPR
       iterator
       _M_insert_multi_node(__node_ptr __hint,
 			   __hash_code __code, __node_ptr __n);
 
       template<typename... _Args>
 
+	_GLIBCXX26_CONSTEXPR
 	std::pair<iterator, bool>
 	_M_emplace_uniq(_Args&&... __args);
 
