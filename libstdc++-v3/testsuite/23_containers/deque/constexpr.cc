4a8a9f15a062eb5ef5ff9e16ee2c71cbec82b92c
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-09-04 09:38:07 -0400

    fix

--- libstdc++-v3/testsuite/23_containers/deque/constexpr.cc
+++ libstdc++-v3/testsuite/23_containers/deque/constexpr.cc
@@ -45,11 +45,11 @@ constexpr bool ctor_tests()
   std::deque<int>dq8 (std::move(dq1));
   VERIFY( dq8 == dq7 );
 
-  std::deque<int, Alloc> dq9 (dq1, alloc);
-  std::deque<int, Alloc> dq10 (std::move(dq1), alloc);
+  std::deque<int, Alloc> dq9 (dq5, alloc);
+  std::deque<int, Alloc> dq10 (std::move(dq5), alloc);
   VERIFY ( dq9 == dq10 );
   std::deque<int, Alloc> dq11 (rg, alloc);
-  VERIFY( dq11 == dq5 );
+  VERIFY( dq11 == dq10 );
 
   return true;
 }
@@ -280,7 +280,7 @@ ranges_test()
 
   rg = {1, 2, 3, 4, 5};
 
-  std::deque<int> dq1;
+  std::deque<int> dq1();
   dq1.insert(dq1.begin() , 1);
   dq1.insert(dq1.end(), 2);
   VERIFY( dq1.size() == 2 );
