ae86f7099b06dff5df2838d2817682fad3b59cfe
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-12-23 19:32:48 -0500

    Catch more copies now. make sure other subsystems play nice with current_return_value

--- gcc/cp/typeck.cc
+++ gcc/cp/typeck.cc
@@ -11490,10 +11490,10 @@ check_return_expr (tree retval, bool *no_warning, bool *dangling)
 
   bool named_return_value_okay_p = want_nrvo_p (bare_retval, functype);
   if (fn_returns_value_p && flag_elide_constructors
-      && current_function_return_value != bare_retval)
+      && !current_function_return_values->contains (bare_retval))
     {
-      if (named_return_value_okay_p
-	  && current_function_return_value == NULL_TREE)
+      if (named_return_value_okay_p)
+//	  && current_function_return_value == NULL_TREE)
 	current_function_return_value = bare_retval;
       else if (current_function_return_value
 	       && VAR_P (current_function_return_value)
