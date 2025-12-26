46f27dd5c85a2e3efc27596f33aa7f2768a8a66e
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-12-26 00:46:44 -0500

    bootstrap maybe oknow

--- gcc/cp/typeck.cc
+++ gcc/cp/typeck.cc
@@ -11491,12 +11491,8 @@ check_return_expr (tree retval, bool *no_warning, bool *dangling)
   bool named_return_value_okay_p = want_nrvo_p (bare_retval, functype);
   if (fn_returns_value_p && flag_elide_constructors
       && (current_function_return_value != bare_retval))
-//      && (current_function_return_values
-//	  && !current_function_return_values->contains (bare_retval)))
     {
       if (named_return_value_okay_p
-//	  && (current_function_return_value
-//	      && !current_function_return_values->contains (bare_retval))
 	  && current_function_return_value == NULL_TREE)
 	current_function_return_value = bare_retval;
       else if (current_function_return_value
@@ -11632,7 +11628,8 @@ check_return_expr (tree retval, bool *no_warning, bool *dangling)
     INIT_EXPR_NRV_P (retval) = true;
     if (!current_function_return_values)
       vec_safe_push (current_function_return_values, bare_retval);
-    else if (!current_function_return_values->contains(bare_retval))
+    else if (!current_function_return_values->contains(bare_retval)
+	     && !current_function_return_values->contains(current_function_return_value))
       vec_safe_push (current_function_return_values, bare_retval);
     current_function_return_value = NULL_TREE;
   }
