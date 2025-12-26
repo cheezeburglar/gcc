23cb6c30b5f6389bce3e5e284a1d4eb16191a6a0
Thor Preimesberger <tcpreimesberger@gmail.com>
2025-12-25 23:39:00 -0500

    More futzing with previous issue

--- gcc/cp/typeck.cc
+++ gcc/cp/typeck.cc
@@ -11490,12 +11490,14 @@ check_return_expr (tree retval, bool *no_warning, bool *dangling)
 
   bool named_return_value_okay_p = want_nrvo_p (bare_retval, functype);
   if (fn_returns_value_p && flag_elide_constructors
-//      && (current_function_return_value != bare_retval
-      && (current_function_return_values
-	  && !current_function_return_values->contains (bare_retval)))
+      && (current_function_return_value != bare_retval))
+//      && (current_function_return_values
+//	  && !current_function_return_values->contains (bare_retval)))
     {
-      if (named_return_value_okay_p)
-//	  && current_function_return_value == NULL_TREE)
+      if (named_return_value_okay_p
+//	  && (current_function_return_value
+//	      && !current_function_return_values->contains (bare_retval))
+	  && current_function_return_value == NULL_TREE)
 	current_function_return_value = bare_retval;
       else if (current_function_return_value
 	       && VAR_P (current_function_return_value)
@@ -11628,7 +11630,11 @@ check_return_expr (tree retval, bool *no_warning, bool *dangling)
   if (current_function_return_value == bare_retval)
   {
     INIT_EXPR_NRV_P (retval) = true;
-    vec_safe_push (current_function_return_values, bare_retval);
+    if (!current_function_return_values)
+      vec_safe_push (current_function_return_values, bare_retval);
+    else if (!current_function_return_values->contains(bare_retval))
+      vec_safe_push (current_function_return_values, bare_retval);
+    current_function_return_value = NULL_TREE;
   }
 
   if (tree set = maybe_set_retval_sentinel ())
