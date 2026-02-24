2eeb81b6c7cfb401abbefa15b2cc83d3144ef8d0
Thor Preimesberger <tcpreimesberger@gmail.com>
2026-02-23 20:33:30 -0500

    testfix

--- libstdc++-v3/testsuite/23_containers/queue/constexpr.cc
+++ libstdc++-v3/testsuite/23_containers/queue/constexpr.cc
@@ -104,9 +104,9 @@ constexpr void ctor_tests()
   VERIFY( q12.front() == 7 );
   q12.pop();
 
-  std::queue<Tp, Container> q13 (std::from_range_t, rg);
+  std::queue<Tp, Container> q13 {std::from_range_t, rg};
   VERIFY ( eq(q13, rg) );
-  std::queue<Tp, Container> q14 (std::from_range_t, rg, Alloc);
+  std::queue<Tp, Container> q14 {std::from_range_t, rg, Alloc};
   VERIFY ( eq(q14, rg) );
 
 }
