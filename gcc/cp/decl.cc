5d6ceebc5808a930632ed59d727d2573e0f88d46
Thor Preimesberger <ThorP@protonmail.com>
2026-06-03 17:26:49 +0100

    move around exp hooks

--- gcc/cp/decl.cc
+++ gcc/cp/decl.cc
@@ -20690,8 +20690,8 @@ finish_function (bool inline_p)
 	}
       }
     }
-  if (current_function_nrv_context)
-    current_function_nrv_context->finalize_nrv_exp(fndecl);
+//  if (current_function_nrv_context)
+//    current_function_nrv_context->finalize_nrv_exp(fndecl);
 //  gcc_assert(current_function_nrv_context);
 
   /* Must mark the RESULT_DECL as being in this function.  */
