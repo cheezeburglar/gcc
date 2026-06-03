4c46e4171acea7b7b97d2a0a424332604c322a41
Thor Preimesberger <ThorP@protonmail.com>
2026-06-03 19:25:05 +0100

    move around exp hooks

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -9821,7 +9821,7 @@ public:
     {
       class nrv_data_exp temp;
       temp.var = r.first;
-      temp.var_corr_rets.copy(r.second);
+      temp.var_corr_rets = r.second.copy();
 //      temp.in_nrv_cleanup = 0;
 //      temp.simple = 0;
 //      nrv_walk_tree(&DECL_SAVED_TREE (fndecl), finalize_nrv_exp_r, &temp, 0);
