de648e612b0d8317be94673f9e4e6b992290f01a
Thor Preimesberger <ThorP@protonmail.com>
2026-06-03 17:06:39 +0100

    gc messing around

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -9829,10 +9829,10 @@ public:
   void finalize_nrv_exp(tree fndecl) {
     for (auto r: all_nrv_candidates)
     {
-      class nrv_data_exp temp;
-      temp.var = r.candidate_bare_retval;
-      temp.var_corr_rets = r.candidate_corresponding_retvals;
-      temp.in_nrv_cleanup = 0;
+//      class nrv_data_exp temp;
+//      temp.var = r.candidate_bare_retval;
+//      temp.var_corr_rets = r.candidate_corresponding_retvals;
+//      temp.in_nrv_cleanup = 0;
 //      temp.simple = 0;
 //      nrv_walk_tree(&DECL_SAVED_TREE (fndecl), finalize_nrv_exp_r, &temp, 0);
       //~r;
