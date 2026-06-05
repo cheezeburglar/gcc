75b1cb8b8f419e69dfff6f6f268d06720803ad01
Thor Preimesberger <ThorP@protonmail.com>
2026-06-05 20:50:15 +0100

    more hook testing

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -2116,7 +2116,7 @@ struct GTY(()) saved_scope {
   vec<cp_omp_begin_assumes_data, va_gc> *omp_begin_assumes;
   vec<cp_omp_declare_variant_attr, va_gc> *omp_declare_variant_attribute;
 
-  nrv_context exp_nrv_context;
+  nrv_context *exp_nrv_context;
 
   struct saved_scope *prev;
 };
