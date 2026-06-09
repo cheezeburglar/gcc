1eea80ba411869db218a5d808ad880c2b3e380e3
Thor Preimesberger <ThorP@protonmail.com>
2026-06-09 04:14:06 +0100

    experimental futzing, hook isnt working like i think

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -9840,6 +9840,7 @@ public:
 
 //      tree outer = outer_curly_brace_block (fndecl);
 //      temp.simple = chain_member(var, BLOCK_VARS (outer));
+      temp.simple = false;
 
       nrv_walk_tree(&DECL_SAVED_TREE (fndecl), finalize_nrv_exp_r, &temp, 0);
       //~r;
