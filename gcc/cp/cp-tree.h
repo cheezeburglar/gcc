8109835e98c542f1feeb3a363c70c6b30d6b13f9
Thor Preimesberger <ThorP@protonmail.com>
2026-06-09 20:40:13 +0100

    experimental futzing, hook isnt working like i think

--- gcc/cp/cp-tree.h
+++ gcc/cp/cp-tree.h
@@ -9825,7 +9825,7 @@ public:
       auto pair = *r;
       tree result = DECL_RESULT (fndecl);
       tree var = pair.first;
-      if (!DECL_NAME(result) {gcc_unreachable();}
+      if (!DECL_NAME(result)) {gcc_unreachable();}
       if (DECL_NAME(result) == DECL_NAME (var))
       {
 	TREE_ADDRESSABLE (result) = TREE_ADDRESSABLE (var);
