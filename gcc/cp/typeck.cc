6dc33cdad7b97fe08e8b359ab213d10e45b3d434
Thor Preimesberger <ThorP@protonmail.com>
2026-06-17 21:49:18 +0100

    debug fail

--- gcc/cp/typeck.cc
+++ gcc/cp/typeck.cc
@@ -11691,8 +11691,8 @@ check_return_expr (tree retval, bool *no_warning, bool *dangling)
 	      && !current_function_return_values->contains (bare_retval))))
     {
       if (named_return_value_okay_p
-	  && ((current_function_return_value == NULL_TREE)
-	     || current_function_return_values))
+	  && ((current_function_return_value == NULL_TREE)))
+//	     || current_function_return_values))
 	current_function_return_value = bare_retval;
       else if (current_function_return_value
 	       && VAR_P (current_function_return_value)
